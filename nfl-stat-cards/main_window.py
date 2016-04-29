305beebef65b2565ffd43459904eca5817f1cfaa
ryanbabida <ryan.babida@gmail.com>
2016-04-29 10:10:50 -0700

    fixing gui popups

--- nfl-stat-cards/main_window.py
+++ nfl-stat-cards/main_window.py
@@ -1,6 +1,5 @@
 from PyQt4 import QtCore, QtGui
 from parse_data import *
-from stat_card import *
 import sys
 
 try:
@@ -16,6 +15,64 @@ try:
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Card(object):
+
+    def __init__(self, Card, current_player): # current_player
+        Card.resize(660, 650)
+        Card.setWindowTitle(current_player[0])
+
+        self.stat_table = QtGui.QTableWidget(Card)
+        self.stat_table.setGeometry(QtCore.QRect(10, 210, 640, 400))
+        self.stat_table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.stat_table.setRowCount(len(current_player[5]) + 1)
+        self.stat_table.setColumnCount(len(current_player[6]))
+
+        for i in range(0, len(current_player[6])):
+            self.stat_table.setItem(0, i, QtGui.QTableWidgetItem(current_player[6][i]))
+        for i in range(1, len(current_player[5]) + 1):
+            for j in range(0, len(current_player[5][0])):
+                self.stat_table.setItem(i, j, QtGui.QTableWidgetItem(current_player[5][i - 1][j]))
+        self.stat_table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+
+        self.name = QtGui.QLabel(Card)
+        self.name.setGeometry(QtCore.QRect(150, 20, 200, 13))
+        self.name.setText(current_player[0])
+        
+        self.num = QtGui.QLabel(Card)
+        self.num.setGeometry(QtCore.QRect(300, 20, 56, 13))
+        self.num.setText(current_player[1] + '-' + current_player[2])
+       
+        self.team = QtGui.QLabel(Card)
+        self.team.setGeometry(QtCore.QRect(375, 20, 300, 13))
+        self.team.setText(current_player[3])
+        
+        self.categories = QtGui.QComboBox(Card)
+        self.categories.setGeometry(QtCore.QRect(500, 10, 104, 26))
+        self.categories.addItem("")
+        for cat in current_player[6]:
+            self.categories.addItem(cat)
+        #self.categories.highlighted['QString'].connect(self.catChanged)
+        #self.categories.currentIndexChanged['QString'].connect(self.selectionchange)
+
+        self.graphicsView_2 = QtGui.QGraphicsView(Card)
+        self.graphicsView_2.setGeometry(QtCore.QRect(150, 40, 500, 161))
+
+        self.photo = QtGui.QLabel(Card)
+        self.photo.setGeometry(QtCore.QRect(10, 50, 130, 150))
+        self.photo.setScaledContents(True)                  #current_player[3]
+        self.photo.setPixmap(QtGui.QPixmap(current_player[4]))
+
+        self.chart_button = QtGui.QPushButton(Card)
+        self.chart_button.setGeometry(610, 12, 30, 20)
+        self.chart_button.setText("Go")
+        self.chart_button.clicked.connect(self.makeChart)
+
+    def makeChart(self):
+        print('here')
+        print (self.categories.currentText())
+
+
 
 class Ui_Form(object):
 
@@ -73,10 +130,8 @@ class Ui_Form(object):
         for name in self.results:
             self.result_list.addItem(name)
 
-
     def getClicked(self):
         # creates the player object
-
         if self.result_list.currentItem() == None: 
             print("Not valid")
         else:
@@ -92,23 +147,22 @@ class Ui_Form(object):
             categories = getCategories(pos[1])
 
             self.current_player = (name, pos[0], pos[1], team, img, stats, categories)
-            
-            #for x in stats:
-            #    print(x)
-
-            '''
-            TODO: Use current player to get    
-             - stats for each game
-             - stats for total
-             - dropdown for each cat
-             - graph based on cat
-             Don't really need player.py...
-            '''
 
             temp = QtGui.QWidget()
             self.cards.append(temp)
-            ui = Ui_Card()
-            ui.setupUi(self.cards[len(self.cards) - 1], self.current_player)
-            self.cards[len(self.cards) - 1].show()
-        
+            ui = Ui_Card(self.cards[len(self.cards) - 1], self.current_player)
+            temp.show()
+
+
+
+if __name__ == "__main__":
+
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
+    
 
--- nfl-stat-cards/stat_card.py
+++ nfl-stat-cards/stat_card.py
@@ -1,34 +1,17 @@
 from PyQt4 import QtCore, QtGui
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
 class Ui_Card(object):
 
-    def setupUi(self, Card, current_player): # current_player
-        Card.setObjectName(_fromUtf8("Card"))
+    def __init__(self, Card, current_player): # current_player
         Card.resize(660, 650)
         Card.setWindowTitle(current_player[0])
 
         self.stat_table = QtGui.QTableWidget(Card)
         self.stat_table.setGeometry(QtCore.QRect(10, 210, 640, 400))
         self.stat_table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        self.stat_table.setObjectName(_fromUtf8("stat_table"))
         self.stat_table.setRowCount(len(current_player[5]) + 1)
         self.stat_table.setColumnCount(len(current_player[6]))
 
-        self.stat_table.setItem(0, 0, QtGui.QTableWidgetItem("Week"))
         for i in range(0, len(current_player[6])):
             self.stat_table.setItem(0, i, QtGui.QTableWidgetItem(current_player[6][i]))
         for i in range(1, len(current_player[5]) + 1):
@@ -38,46 +21,40 @@ class Ui_Card(object):
 
         self.name = QtGui.QLabel(Card)
         self.name.setGeometry(QtCore.QRect(150, 20, 200, 13))
-        self.name.setObjectName(_fromUtf8("name"))
         self.name.setText(current_player[0])
         
         self.num = QtGui.QLabel(Card)
-        self.num.setGeometry(QtCore.QRect(350, 20, 56, 13))
-        self.num.setObjectName(_fromUtf8("num"))
+        self.num.setGeometry(QtCore.QRect(300, 20, 56, 13))
         self.num.setText(current_player[1] + '-' + current_player[2])
        
         self.team = QtGui.QLabel(Card)
-        self.team.setGeometry(QtCore.QRect(425, 20, 300, 13))
-        self.team.setObjectName(_fromUtf8("team"))
+        self.team.setGeometry(QtCore.QRect(375, 20, 300, 13))
         self.team.setText(current_player[3])
         
         self.categories = QtGui.QComboBox(Card)
-        self.categories.setGeometry(QtCore.QRect(550, 10, 104, 26))
-        self.categories.setObjectName(_fromUtf8("comboBox"))
+        self.categories.setGeometry(QtCore.QRect(500, 10, 104, 26))
+        self.categories.addItem("")
         for cat in current_player[6]:
             self.categories.addItem(cat)
         #self.categories.highlighted['QString'].connect(self.catChanged)
-        self.categories.currentIndexChanged['QString'].connect(self.selectionchange)
+        #self.categories.currentIndexChanged['QString'].connect(self.selectionchange)
 
         self.graphicsView_2 = QtGui.QGraphicsView(Card)
         self.graphicsView_2.setGeometry(QtCore.QRect(150, 40, 500, 161))
-        self.graphicsView_2.setObjectName(_fromUtf8("graphicsView_2"))
 
         self.photo = QtGui.QLabel(Card)
         self.photo.setGeometry(QtCore.QRect(10, 50, 130, 150))
-        self.photo.setObjectName(_fromUtf8("photo"))
         self.photo.setScaledContents(True)                  #current_player[3]
         self.photo.setPixmap(QtGui.QPixmap(current_player[4]))
 
-        QtCore.QMetaObject.connectSlotsByName(Card)
+        self.chart_button = QtGui.QPushButton(Card)
+        self.chart_button.setGeometry(610, 12, 30, 20)
+        self.chart_button.setText("Go")
+        self.chart_button.clicked.connect(self.makeChart)
 
-
-    def selectionchange(self,i):
-        print ("Items in the list are :")
-        
-        for count in range(self.cb.count()):
-            print (self.cb.itemText(count))
-        print ("Current index",i,"selection changed ",self.cb.currentText())
+    def makeChart(self):
+        print('here')
+        print (self.categories.currentText())
 
 
 
